19e678d2d30dc12457b93f5c5ce5f69164621c21
jacobo1407b <jacobohernandezmendieta7@gmail.com>
2021-11-09 22:49:54 -0600

    add transfer

--- contracts/AssetT.sol
+++ contracts/AssetT.sol
@@ -4,7 +4,8 @@ pragma solidity >=0.4.22 <0.9.0;
 contract AssetT{
     //Structs
     uint id_prop;
-
+    
+    
     struct Property{
         address user;
         bool visible;
@@ -32,11 +33,11 @@ contract AssetT{
         user[msg.sender] = _user;
         isUser[msg.sender] = true;
         emit UserRegistered(msg.sender, _user);
-    }
+    }//funciona
     
     function getUser() public view returns(string memory){
         return user[msg.sender];
-    }
+    }//funciona
     
     /*function registerProperty(string memory _uuid) public {
         require(isUser[msg.sender] == true,"Issuer not registered to register a certificate");
@@ -59,26 +60,40 @@ contract AssetT{
          issuerPropertyRegister[msg.sender].push(id);
          emit propertyIssued(id,msg.sender, _uuid);
         
-    }
+    }//funciona
     
     function getIdProperty(uint _id) public view returns(Property memory){
         Property memory prop = propertyIdentifier[_id];
         return prop;
-    }
-    
+    }//funciona
+
     function getProperty() public view returns(uint[] memory){
         return issuerPropertyRegister[msg.sender];
-    }
+    }//funciona
     
     function onVisible(bool _visible,uint _id) public {
         Property memory temp = propertyIdentifier[_id];
         temp.visible = _visible;
         propertyIdentifier[_id] = temp;
-    }
+         emit propertyIssued(_id,msg.sender, temp.document_hash);
+    }//funciona
     
     function onUpdatePrice(uint _id, uint256 _newPrice) public{
         Property memory temp = propertyIdentifier[_id];
         temp.price = _newPrice;
         propertyIdentifier[_id] = temp;
+        emit propertyIssued(_id,msg.sender, temp.document_hash);
+    }//funciona
+    
+    function payProperty(address _newowner,uint _id,string memory _newipfs) public{
+        Property memory temp = getIdProperty(_id);
+        uint [] memory issuer =  issuerPropertyRegister[msg.sender];
+        temp.user = _newowner;
+        temp.visible = false;
+        temp.document_hash = _newipfs;
+        propertyIdentifier[_id]=temp;
+        issuer[_id]=0;
+        issuerPropertyRegister[_newowner].push(_id);
+        emit propertyIssued(_id,_newowner, temp.document_hash);
     }
 }
