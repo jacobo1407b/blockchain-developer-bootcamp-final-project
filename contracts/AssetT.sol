b8476ba96a90c475ebeec65fde34063237642a97
jacobo1407b <jacobohernandezmendieta7@gmail.com>
2021-11-22 22:05:44 -0600

    documentar funcionws

--- contracts/AssetT.sol
+++ contracts/AssetT.sol
@@ -2,10 +2,11 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract AssetT{
-    //Structs
+    //counter id
     uint id_prop;
     
-    
+    //Structs
+    /**struct for propertis */
     struct Property{
         address user;
         bool visible;
@@ -29,37 +30,39 @@ contract AssetT{
     event propertyIssued(uint indexed property,address indexed user, string indexed document);
     
     /* Modificadores */
-    
+    /** check if a user exists */
      modifier isRegisterUser(){
          require((isUser[msg.sender] == false), "Este usuario ya esta registrado");
          _;
-     }//funciona
+     }//works
      
      modifier isUserActive(){
          require(isUser[msg.sender] == true,"Este usuario no existe");
          _;
-     }//funciona
+     }//works
+     /**authorization */
      modifier isAutorize(address owner){
          require(owner == msg.sender, "403");
          _;
-     }//funciona
+     }//works
      
-    
+    /**register a user, an identifier is required */
     function registerUser(string memory _user) public isRegisterUser(){
         user[msg.sender] = _user;
         isUser[msg.sender] = true;
         emit UserRegistered(msg.sender, _user);
-    }//funciona
-    
+    }//works
+    /**function returned by a user */
     function getUser() public view returns(string memory){
         return user[msg.sender];
-    }//funciona
+    }//works
     
     /*function registerProperty(string memory _uuid) public {
         require(isUser[msg.sender] == true,"Issuer not registered to register a certificate");
         issuerProperty[_uuid] = msg.sender;
         emit PropertyRegister(msg.sender, _uuid);
     }*/
+    /**register a property, requires a hash of ipfs, name and price */
     function emitRegisterProperty(string memory _uuid, string memory _img_uid,string memory _name,uint256 _value) public isUserActive(){
         
         Property memory proper;
@@ -78,6 +81,7 @@ contract AssetT{
         
     }//funciona
     
+    /**get a property by id, the function is validated with authorization */
     function getIdProperty(uint _id) public view isAutorize(issuerProperty[_id]) 
       returns(address  direccion,bool visible,uint256 price, string memory ipfs,string memory name, string memory document){
        Property memory prop = propertyIdentifier[_id];
@@ -92,17 +96,18 @@ contract AssetT{
        return (direccion,visible,price,ipfs,name,document);
     }//funciona
 
+    /**get an array of properties for a user */
     function getProperty() public view returns(uint[] memory){
         return issuerPropertyRegister[msg.sender];
     }//funciona
-    
+    /**update the visibility of a property */
     function onVisible(bool _visible,uint _id) public isAutorize(issuerProperty[_id]){
         Property memory temp = propertyIdentifier[_id];
         temp.visible = _visible;
         propertyIdentifier[_id] = temp;
          emit propertyIssued(_id,msg.sender, temp.document_hash);
     }//funciona
-    
+    /**update the price of a property */
     function onUpdatePrice(uint _id, uint256 _newPrice) public isAutorize(issuerProperty[_id]){
         Property memory temp = propertyIdentifier[_id];
         temp.price = _newPrice;
